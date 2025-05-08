fb11441d9df20a719c5380e32d360f6d20f7a328
WaqarMajiet97 <majietwaqar97@gmail.com>
2025-05-08 14:55:32 +0200

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -42,7 +42,7 @@ const About = () => {
             <div className="prose max-w-none">
               
               <p className="text-gray-700 mb-4">
-                IT Professional focussing on Cloud Solutions Development on various platforms such as AWS, Google Cloud and Microsoft Azure. Experience in languages such as C#, JAVA, Python, HTML. Also. Web Development and Desktop Application Development.
+                IT Professional focussing on Cloud Solutions Development on various platforms such as AWS, Google Cloud and Microsoft Azure. Experience in languages such as C#, JAVA, Python, HTML. Also, Web Development and Desktop Application Development.
               </p>
             
             </div>
